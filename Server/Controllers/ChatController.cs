6fa1c6c26d588b8e1c85c49db41512c37c60abb3
Trollerolle <tjtr63311@edu.ucl.dk>
2025-03-18 12:56:57 +0100

    First commit

---
+++ Server/Controllers/ChatController.cs
@@ -0,0 +1,35 @@
+﻿using BlazorChatApp.Shared;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BlazorChatApp.Server.Controllers
+{
+
+	[ApiController]
+	[Route("api/[controller]")]
+	public class ChatController : ControllerBase
+	{
+		private static List<ChatMessage> messages = new List<ChatMessage>();
+
+		[HttpGet]
+		public IEnumerable<ChatMessage> GetMessages()
+		{
+			return messages.OrderBy(m => m.Timestamp);
+		}
+
+		[HttpPost]
+		public IActionResult PostMessage([FromBody] ChatMessage message)
+		{
+			if (message == null || string.IsNullOrWhiteSpace(message.User) || string.IsNullOrWhiteSpace(message.Message))
+			{
+				return BadRequest("Invalid message.");
+			}
+
+			message.Timestamp = DateTime.UtcNow;
+			messages.Add(message);
+			return Ok();
+		}
+	}
+}
---
+++ Shared/ChatMessage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorChatApp.Shared
+{
+	public class ChatMessage
+	{
+		public string User { get; set; } = string.Empty;
+		public string Message { get; set; } = string.Empty;
+		public DateTime Timestamp { get; set; }
+	}
+}
